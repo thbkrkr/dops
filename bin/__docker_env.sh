968878a673692629690809f780123894179ae4f1
thbkrkr <thb.krkr@gmail.com>
2016-04-23 12:10:38 +0200

    Install doo

--- bin/__docker_env.sh
+++ bin/__docker_env.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 #
-# Set up the environment for the Docker client using 2 variables:
-# - ENV
-# - MACHINE
+# Set up the environment for the Docker client.
+#
+# You can set the Docker remote host using the
+# env var MACHINE.
+#
+# Or the Docker remote host is automatically discovered.
+# If swarm master exists, it is used else the first node is used.
 #
 
 ERROR=""
 IS_ERROR=0
-MACHINE_STORAGE_PATH=${MACHINE_STORAGE_PATH:-~/.docker/machine}
-
-[[ ! -d $MACHINE_STORAGE_PATH ]] &&
-    ERROR="error: directory $MACHINE_STORAGE_PATH not found" && IS_ERROR=1
 
 SWARM="false"
 
 get_swarm_master() {
+  if [[ ! -d $MACHINE_STORAGE_PATH ]]; then
+    echo "{}"
+    return
+  fi
+
   find $MACHINE_STORAGE_PATH/machines -name config.json \
     | xargs -n1 \
       jq -c '{"machine":.Driver.MachineName,"master":.HostOptions.SwarmOptions.Master}
       | select(.master==true)'
 }
 
-default_machine() {
-  MACHINE=""
+set_machine() {
+  [[ ! -z $MACHINE ]] && return
 
   # Try to get a swarm master
   if [[ -d $MACHINE_STORAGE_PATH/machines ]]; then
@@ -30,13 +35,12 @@ default_machine() {
     SWARM=$(get_swarm_master | jq -r .master)
   fi
 
-  # Get the first node
+  # Get the first node if no swarm master
   [[ "$MACHINE" == "" ]] && \
-  [[ -d $MACHINE_STORAGE_PATH ]] &&
-    MACHINE=$(docker-machine ls -q | head -1)
+  MACHINE=$(docker-machine ls -q | head -1)
 }
 
-valid_args() {
+xxxxxvalid_args() {
   ENV_MACHINE=${MACHINE:-""}
   ARG_MACHINE=${1:-""}
 
@@ -76,16 +80,20 @@ valid_args() {
 }
 
 main() {
-  valid_args "$@"
+  #valid_args "$@"
+  set_machine
 
   set -u
 
   # Set up the environment for the Docker client
   # Speed up the setup by exporting directly the variables without the 'docker-machine env' command
 
-  if [[ "$IS_ERROR" -eq 1 ]] ; then
-    echo 'export ERROR="'"$ERROR"'. No docker environment set!"'
-  else
+  # if [[ "$IS_ERROR" -eq 1 ]] ; then
+  #   echo 'export ERROR="'"$ERROR"'. No docker environment set!"'
+  # else
+  if [[ "$MACHINE" == "" ]]; then
+    return
+  fi
     PORT=2376
     if [[ "$SWARM" == "true" ]]; then
       PORT=3376
@@ -97,7 +105,7 @@ main() {
 export DOCKER_HOST="tcp://'$IP':'$PORT'"
 export DOCKER_CERT_PATH="'$MACHINE_STORAGE_PATH'/machines/'$MACHINE'"
 export DOCKER_MACHINE_NAME="'$MACHINE'"'
-  fi
+  # fi
 }
 
 main "$@"
